8e3b6c6dd71dcacf4c4616835d31a33e34c86039
Ivan Smirnov <smirnovrichi@gmail.com>
2019-01-12 12:36:22 +0300

    Added simple anonymous login (#16)
    * simple anonymous login
    
    * fix loading state

--- client/App.tsx
+++ client/App.tsx
@@ -1,49 +1,33 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
-import * as firebase from 'firebase/app';
-import ApolloClient from "apollo-boost";
 import { ApolloProvider } from 'react-apollo';
-import gql from "graphql-tag";
-import { dodayStore, Doday } from './stores/doday-store';
+import client from './graphs/client';
+import { dodayStore, Doday, authStore } from './stores';
 import 'firebase/auth';
 import './App.scss';
-
-const client = new ApolloClient({
-  uri: "http://localhost:4000"
-});
-
-client
-  .query({
-    query: gql`
-      {
-        Hero(id: 1) {
-          name
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
 
 @observer
 class App extends React.Component {
   componentDidMount() {
-    if (!firebase.auth().currentUser) {
-      firebase.auth().signInAnonymously()
-        .then(() => {
-          
-        })
-        .catch(function (error) {
-          // Handle Errors here.
-        });
+    authStore.listenAuthChange();
+  }
+
+  renderContent = () => {
+    if (authStore.currentHero === undefined) {
+      return <div>Loading...</div>
+    } else if (authStore.currentHero === null) {
+      return <button onClick={() => authStore.loginAnonymously()}>Login Anonymously</button>;
+    } else {
+      return <div className="App">
+        {dodayStore.dodays.map((doday: Doday) => (<li key={doday.id} onClick={() => dodayStore.toggleDoday(doday.id)}>{doday.title} | {`${doday.completed}`}</li>))}
+      </div>;
     }
   }
 
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="App">
-          {dodayStore.dodays.map((doday: Doday) => (<li key={doday.id} onClick={() => dodayStore.toggleDoday(doday.id)}>{doday.title} | {`${doday.completed}`}</li>))}
-        </div>
+        { this.renderContent() }
       </ApolloProvider>
     );
   }
---
+++ client/stores/auth-store.ts
@@ -0,0 +1,31 @@
+import { observable, action, computed } from 'mobx';
+import * as firebase from 'firebase/app';
+
+export class AuthStore {
+  @observable private _hero: firebase.User | undefined | null = undefined;
+
+  @computed
+  get currentHero(): firebase.User | undefined | null {
+    return this._hero;
+  }
+
+  @action
+  loginAnonymously() {
+    firebase.auth().signInAnonymously();
+  }
+
+  @action
+  listenAuthChange() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // User is signed in.
+        this._hero = user;
+      } else {
+        // User is signed out.
+        this._hero = null;
+      }
+    });
+  }
+}
+
+export const authStore = new AuthStore();
