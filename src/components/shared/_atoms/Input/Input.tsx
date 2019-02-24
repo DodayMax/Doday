6b8f7fd076f6bfa6e2da57f2c7526b11fc514639
Ivan Smirnov <v-ivsmir@microsoft.com>
2019-02-24 18:07:09 +0300

    input stories

--- src/components/shared/_atoms/Input/Input.tsx
+++ src/components/shared/_atoms/Input/Input.tsx
@@ -1,38 +1,50 @@
 import * as React from 'react';
+import classnames from 'classnames';
+import { Icons } from '@root/components';
+
+const styles = require('./_input.module.scss');
 
 interface InputProps {
+  isLoading?: boolean;
   autofocus?: boolean;
 }
 
 export class Input extends React.Component<InputProps & React.InputHTMLAttributes<HTMLInputElement>> {
-  input: React.RefObject<HTMLInputElement>;
+  input: HTMLInputElement | null;
 
   constructor(props) {
     super(props);
 
-    this.input = React.createRef();
+    this.input = null;
   }
 
   componentDidMount() {
-    if (this.props.autofocus && this.input && this.input.current) {
+    if (this.props.autofocus && this.input) {
       setTimeout(
-        () => this.input.current!.focus(),
+        () => this.input!.focus(),
         100
       );
     }
   }
 
   render() {
-    const { value, onChange, autofocus, ...props } = this.props;
+    const { value, onChange, autofocus, isLoading = false, ...props } = this.props;
+    const classNames = classnames({
+      [styles.input]: true,
+      [styles.loading]: isLoading,
+    });
 
     return (
-      <input
-        className="input"
-        ref={this.input}
-        value={value}
-        onChange={onChange}
-        {...props}
-      />
+      <div className={styles.inputContainer}>
+        <input
+          className={classNames}
+          ref={(node) => this.input = node}
+          value={value}
+          onChange={onChange}
+          {...props}
+        />
+        {isLoading ? <Icons.InlineLoader className={styles.loader} /> : null}
+      </div>
     );
   }
 }
--- src/components/shared/_atoms/icons/inline-loader/inline-loader.tsx
+++ src/components/shared/_atoms/icons/inline-loader/inline-loader.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 
-export const InlineLoader = () => (
-  <svg version="1.1" id="inline-loader" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+interface InlineLoaderProps {
+  className: string;
+}
+
+export const InlineLoader = (props: InlineLoaderProps) => (
+  <svg version="1.1" id="inline-loader" {...props} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
     width="24px" height="30px" viewBox="0 0 24 30" xmlSpace="preserve">
     <rect x="0" y="13" width="4" height="5" fill="#333">
       <animate attributeName="height" attributeType="XML"
